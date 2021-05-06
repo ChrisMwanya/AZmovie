8172dc34c3c6b0aaa2bbe2d17c039e0fe3d63ca8
ChrisMwanya <cmwanya@gmail.com>
2021-05-06 15:52:31 +0100

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-
-import NavBar from './components/navbar';
-import styled from 'styled-components';
-import Header from './components/headers/header';
-import Main from './components/mains/main';
-import Footer from './components/footer/footer';
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navbar";
+import styled from "styled-components";
+import Header from "./components/headers/header";
+import Main from "./components/mains/main";
+import Footer from "./components/footer/footer";
 
 const MainWrapper = styled.div`
-  width: 100vw;
-  margin: 0 auto;
-`
+	width: 100vw;
+	margin: 0 auto;
+`;
 function App() {
-  return (
-    <MainWrapper>
-      <NavBar />
-      <Header />
-      <Main/>
-      <Footer/>
-     </MainWrapper>
-
-  );
+	return (
+		<MainWrapper>
+			<NavBar />
+			<Switch>
+				<Header />
+				<Main />
+				<Footer />
+			</Switch>
+		</MainWrapper>
+	);
 }
 
 export default App;
